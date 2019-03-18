2a308fb5e1b8935185f31ffe0607cc7818883274
TheawesomeQ <TheawesomeQ@users.noreply.github.com>
2019-03-18 16:49:52 -0400

    Messed with modules, created tasklist and began work on recyclerview

---
+++ ultimatetodolist3/src/main/java/com/example/ultimatetodolist3/Task.java
@@ -0,0 +1,9 @@
+package com.example.ultimatetodolist3;
+import java.time.LocalDateTime;
+
+public class Task {
+    String title;
+    String description;
+    float duration;
+    java.time.LocalDateTime dueDate;
+}
